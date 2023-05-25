18aa71e2655852071e6e4fd8c3850f4835d56791
Plat01 <fplatoff@gmail.com>
2023-05-25 19:16:02 +0800

    Add all setups for final task

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import asyncio
-
 import discord
 from discord.ext import commands
 from app.config import TOKEN
-# from app import tasks, cmds, events
-# import tracemalloc
-#
-# tracemalloc.start()
 
 
 intents = discord.Intents.all()
@@ -18,27 +12,15 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}")
     # Load command modules
     await bot.load_extension("app.cmds")
-    # await bot.wait_until_ready()
-    # tasks.send_task.start()
-    channel = bot.get_channel(1094300838580728002)
-    print(channel)
+    # start task loop
     send_task.start()
-    # await channel.send("sqwe")
-
-
-
-@bot.event
-async def on_shard_resumed(shard_id):
-    print(f"Shard {shard_id} has resumed.")
-# @bot.event
-# async def on_shard_ready(1094300838580728002):
-#     tasks.send_task.start()
 
 
 if __name__ == '__main__':
     from discord.ext import tasks
 
-
+    #  loops don't work from separate file because  of discord.py asynchronous architecture,
+    #  so I put it here
     @tasks.loop(seconds=24)
     async def send_task():
         print(f"Exercises task is on on channel ")
