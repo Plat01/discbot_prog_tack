2683f65e16da72adba9f4b3d62e22f8b709f9a49
Plat01 <fplatoff@gmail.com>
2023-05-26 22:07:35 +0800

    Add requirements

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This bot uploads a task to your channel every hour.
 
 Tasks and tests (no more than 3) are loaded into the table manually.
 
-For start the bot add token of your bot as "TOKEN" and id of your chanal as "CHANEL_ID" to envaerment variables. Then start bot using "python3 main.py" command.
+For start the bot add token of your bot as "TOKEN" and id of your canal as "CHANEL_ID" to endangerment variables. 
+Then start bot using "python3 main.py" command.
 
-Bot use "discord.py" library and "aiosqlite" for asynchronus access to SQLite database.
+Bot use "discord.py" library and "aiosqlite" for asynchronous access to SQLite database.
--- main.py
+++ main.py
@@ -29,30 +29,6 @@ async def on_ready():
     send_task.start()
 
 
-# @bot.event
-# async def on_message(message):
-#     # Check if the message author is a member of a guild (server)
-#     if message.guild:
-#         member = message.guild.get_member(message.author.id)
-#         if member:
-#             # Accessing member information
-#             username = member.name  # Username of the member
-#             nickname = member.nick  # Nickname of the member (if set)
-#             discriminator = member.discriminator  # Member's discriminator
-#             # avatar_url = member.avatar_url  # URL of the member's avatar
-#             joined_at = member.joined_at  # Date and time the member joined the server
-#             roles = member.roles  # List of roles the member has
-#             permissions = member.guild_permissions  # Member's permissions in the server
-#
-#             # Printing member information
-#             print(f"Username: {username}")
-#             print(f"Nickname: {nickname}")
-#             print(f"Discriminator: {discriminator}")
-#             print(f"Joined At: {joined_at}")
-#             print(f"Roles: {roles}")
-#             print(f"Permissions: {permissions}")
-
-
 #  loops don't work from separate file because  of discord.py asynchronous architecture,
 #  so I put it here
 @tasks.loop(hours=24)
@@ -66,5 +42,4 @@ async def send_task():
 
 
 if __name__ == '__main__':
-
     bot.run(token=TOKEN)
